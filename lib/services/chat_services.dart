6a76a23724dbd19f2a09cf6f72ecda42a4787636
joselito <joselitorebello20@gmail.com>
2024-02-12 00:23:03 +0530

    chat service

---
+++ lib/services/chat_services.dart
@@ -0,0 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fitness_social_app/models/message.dart';
+
+class ChatService {
+  // Get instance of firestore
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // get user stream
+  Stream<List<Map<String, dynamic>>> getUsersStream() {
+    return _firestore.collection("users").snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        final user = doc.data();
+        return user;
+      }).toList();
+    });
+  }
+
+  // send message
+
+  Future<void> sendMessage(String receiverID, message) async {
+    // get current user info
+
+    final String currentUserID = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email!;
+    final Timestamp timeStamp = Timestamp.now();
+
+    // create a new message
+    Message newMessage = Message(
+        senderID: currentUserID,
+        senderEmail: currentUserEmail,
+        receiverID: receiverID,
+        message: message,
+        timestamp: timeStamp);
+
+    // construct chat room id for the two users ( sorted to ensure uniqueness)
+    List<String> ids = [currentUserID, receiverID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+
+    // add new messages
+    await _firestore
+        .collection("chat_rooms")
+        .doc(chatRoomID)
+        .collection("messages")
+        .add(newMessage.toMap());
+  }
+
+  // get messages
+  Stream<QuerySnapshot> getMessages(String userID, otherUserID) {
+    // construct a chatroom ID for the two users
+    List<String> ids = [userID, otherUserID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+
+    return _firestore
+        .collection("chat_rooms")
+        .doc(chatRoomID)
+        .collection("messages")
+        .orderBy("timestamp", descending: false)
+        .snapshots();
+  }
+}
